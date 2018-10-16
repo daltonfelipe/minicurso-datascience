59947976a5b877f30812c6548d86c7ffb60f68c0
Dalton Felipe <daltonphellipe@gmail.com>
2018-10-16 08:05:38 -0300

    update files

--- scripts/str_ex01.py
+++ scripts/str_ex01.py
@@ -6,5 +6,17 @@ Created on Mon Oct 15 17:15:38 2018
 @author: dalton
 """
 
-nome = "dalton"
-print(len(nome))
+notas = []
+
+for i in range(3):
+    notas.append(int(input("Digite nota %d: "%(i+1))))
+
+media = sum(notas)/3
+
+if media>=6 and media<10:
+    print("Media: {}\tStatus: {}".format(media, "aprovado"))
+elif media == 10.0:
+    print("Media: {}\tStatus: {}".format(media, "excelente"))
+else:
+    print("Media: {}\tStatus: {}".format(media, "substitutiva"))
+    
